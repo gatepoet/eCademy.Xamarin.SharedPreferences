31f77a857c134ff7105d81257dce469eb4c7150c
kristoffer.deinoff <kristoffer.deinoff@gmail.com>
2018-01-15 20:41:55 +0100

    Add project files.

---
+++ eCademy.Xamarin.SharedPreferences/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace eCademy.Xamarin.SharedPreferences
+{
+    [Activity(Label = "eCademy.Xamarin.SharedPreferences", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
